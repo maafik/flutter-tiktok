af2d571e945baf6e73cb2081adebb4e3fcece40e
maafik <maafik@66.ru>
2025-07-15 14:36:05 +0300

    322

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_flutter/screens/feed_screen.dart';
 import 'package:tiktok_flutter/service_locator.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
